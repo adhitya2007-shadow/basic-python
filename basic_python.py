eacd2a84605e6a93ef6c803bdbb24b67f53d1314
REMANI ADHITYA HARSHITH <adhitya.r1982@gmail.com>
2025-09-13 09:46:35 +0530

    Add files via upload

---
+++ basic_python.py
@@ -0,0 +1,36 @@
+#      Program to print twinkle twinkle little star poem'''
+#                       ALT+SHIFT'''
+'''
+print("Twinkle, twinkle, little star,")
+print("I wonder what you are!")
+print("bove the world so high,")
+print(" a diamond in the sky.")
+
+print(" the blazing sun is gone,")
+print(" he nothing shines upon,")
+print(" you show your little light,")
+print("twinkle, twinkle, all the night.")
+
+print("when the traveler in the dark")
+print("thanks you for your tiny spark,")
+print("could he see where to go,")
+print("if you did not twinkle so?")
+
+print("then the dark blue sky you keep,")
+print("oftenn through my curtains peep")
+print("you never shut your eye,")
+print(" the sun is in the sky.")
+
+print("our bright and tiny spark")
+print("lights the traveler in the dark,")
+print("though I know not what you are,")
+print("Twinkle, twinkle, little star.")
+print("-"*50)
+'''
+
+
+
+
+
+
+
